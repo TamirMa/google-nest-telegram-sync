334d0a90b289e7e9692f650b7156e636ffb57216
KneelStar <neelshah603@gmail.com>
2024-12-26 06:08:54 -0500

    icons

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
+import sys
 import tkinter as tk
 from app_prefs_database import check_database_exists
 from auth_app_ui import GoogleNestClipperApp
 from pre_auth_app_ui import PreAuthGoogleNestClipperApp
 
+def resource_path(relative_path):
+    """Get the absolute path to a resource, works for dev and for PyInstaller."""
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
 if __name__ == "__main__":
     root = tk.Tk()
+    icon_path = resource_path("resources/Nest Clipper Logo.png")
+    icon_image = tk.PhotoImage(file=icon_path)
+    root.iconphoto(False, icon_image)
 
     if check_database_exists(): # this should also check if master token is valid. not sure how to do that
         app = GoogleNestClipperApp(root)
