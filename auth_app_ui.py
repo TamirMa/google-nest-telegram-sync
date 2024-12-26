e50857762492e0c5880e895c73236e1f3de4938e
KneelStar <neelshah603@gmail.com>
2024-12-26 06:57:00 -0500

    no messing with master token from ui

--- auth_app_ui.py
+++ auth_app_ui.py
@@ -14,7 +14,7 @@ class NestClipperApp:
         self.db_handler = DatabaseHandler(get_db_path())
 
         self.root.title("Nest Clipper")
-        self.root.geometry("580x370")
+        self.root.geometry("570x330")
         self.root.configure(bg="#f2f2f2")
         self.initialize_ui()
         self.load_app_prefs()
@@ -36,47 +36,39 @@ class NestClipperApp:
         form_frame = tk.Frame(self.root, bg="#f2f2f2", padx=20, pady=20)
         form_frame.pack(fill="both", expand=True)
 
-        # Email
+        # Email (Read-only)
         tk.Label(form_frame, text="Email:", font=("Arial", 12), bg="#f2f2f2").grid(
             row=0, column=0, sticky="w", pady=10
         )
-        self.email_entry = tk.Entry(form_frame, width=40, font=("Arial", 12))
+        self.email_entry = tk.Entry(form_frame, width=40, font=("Arial", 12), state="readonly")
         self.email_entry.grid(row=0, column=1, pady=10)
-
-        # Master Token
-        tk.Label(
-            form_frame, text="Master Token:", font=("Arial", 12), bg="#f2f2f2"
-        ).grid(row=1, column=0, sticky="w", pady=10)
-        self.master_token_entry = tk.Entry(
-            form_frame, show="*", width=40, font=("Arial", 12)
-        )
-        self.master_token_entry.grid(row=1, column=1, pady=10)
 
         # Video Save Path
         tk.Label(
             form_frame, text="Video Save Path:", font=("Arial", 12), bg="#f2f2f2"
-        ).grid(row=2, column=0, sticky="w", pady=10)
+        ).grid(row=1, column=0, sticky="w", pady=10)
         path_frame = tk.Frame(form_frame, bg="#f2f2f2")
-        path_frame.grid(row=2, column=1, sticky="w")
+        path_frame.grid(row=1, column=1, sticky="w")
         self.video_save_path_entry = tk.Entry(
             path_frame, width=30, font=("Arial", 12), state="readonly"
         )
         self.video_save_path_entry.pack(side=tk.LEFT, padx=5)
-        tk.Button(
+        self.browse_button = tk.Button(
             path_frame,
             text="Browse...",
             command=self.select_save_path,
             font=("Arial", 10),
             bg="#4CAF50",
             fg="white",
-        ).pack(side=tk.RIGHT)
+        )
+        self.browse_button.pack(side=tk.RIGHT)
 
         # Time to Refresh
         tk.Label(
             form_frame, text="Refresh Freq (sec):", font=("Arial", 12), bg="#f2f2f2"
-        ).grid(row=3, column=0, sticky="w", pady=10)
+        ).grid(row=2, column=0, sticky="w", pady=10)
         self.time_to_refresh_entry = tk.Entry(form_frame, width=40, font=("Arial", 12))
-        self.time_to_refresh_entry.grid(row=3, column=1, pady=10)
+        self.time_to_refresh_entry.grid(row=2, column=1, pady=10)
 
         # Start/Stop Button
         self.start_stop_button = tk.Button(
@@ -105,8 +97,9 @@ class NestClipperApp:
     def load_app_prefs(self):
         nest_clipper_prefs = self.db_handler.get_app_prefs()
         if nest_clipper_prefs:
+            self.email_entry.config(state="normal")
             self.email_entry.insert(0, nest_clipper_prefs.get("USERNAME", ""))
-            self.master_token_entry.insert(0, nest_clipper_prefs.get("MASTER_TOKEN", ""))
+            self.email_entry.config(state="readonly")
             self.video_save_path_entry.config(state="normal")
             self.video_save_path_entry.insert(0, nest_clipper_prefs.get("VIDEO_SAVE_PATH", ""))
             self.video_save_path_entry.config(state="readonly")
@@ -144,18 +137,15 @@ class NestClipperApp:
 
     def save_app_prefs(self):
         email = self.email_entry.get()
-        master_token = self.master_token_entry.get()
         video_save_path = self.video_save_path_entry.get()
         time_to_refresh = self.time_to_refresh_entry.get()
 
-        if not email or not master_token or not video_save_path or not time_to_refresh:
+        if not email or not video_save_path or not time_to_refresh:
             messagebox.showerror("Error", "All fields are required.")
             return False
 
         nest_clipper_prefs = {
             "USERNAME": email,
-            "MASTER_TOKEN": master_token,
-            "MASTER_TOKEN_CREATION_DATE": datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
             "VIDEO_SAVE_PATH": video_save_path,
             "TIME_TO_REFRESH": time_to_refresh,
         }
@@ -177,9 +167,11 @@ class NestClipperApp:
                 self.start_periodic_task()
 
     def set_fields_state(self, state):
-        self.email_entry.config(state=state)
-        self.master_token_entry.config(state=state)
         self.time_to_refresh_entry.config(state=state)
+        if state == "readonly":
+            self.browse_button.config(state="disabled", bg="#312f2f")
+        else:
+            self.browse_button.config(state="normal", bg="#4CAF50")
 
     def start_periodic_task(self):
         def task():
@@ -198,7 +190,6 @@ class NestClipperApp:
                     self.timer_thread.start()
 
         task()
-
 
 if __name__ == "__main__":
     root = tk.Tk()
